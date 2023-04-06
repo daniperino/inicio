fc05378a4f68b1b17253ba0746ea1395e354a772
daniperino <daniperino@gmail.com>
2023-04-06 17:40:30 -0300

    testes de erro

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,22 @@
+{
+  "reporter"; "mochawesome",
+  "reportarOptions"; {
+    "reportDir"; "cypress/report/mochawesome-report",
+    "overwrite"; true,
+    "html"; true,
+    "json"; false,
+    "timestamp"; "mmddyyyy_HHMMss"
+  }
+} 
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '3bmqne',
   e2e: {
     baseUrl: 'https://www.alura.com.br/'
   },
+
 });
+
+
